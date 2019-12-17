2da5e507d47a1c105bd3ef912c9fa66fff0b55ea
marutakan <red.scarlet.crimson@gmail.com>
2019-12-17 17:50:20 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
+# ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true,
+             admin:     true,
              activated: true,
              activated_at: Time.zone.now)
 
@@ -14,12 +15,21 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-              activated: true,
-              activated_at: Time.zone.now)
+               activated: true,
+               activated_at: Time.zone.now)
 end
 
+# マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
